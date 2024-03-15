8691cd13d6b196e6fcd7dafc73d5cac756b88502
ytm15 <163555831+ytm15@users.noreply.github.com>
2024-03-15 18:51:34 +0300

    Add files via upload

---
+++ app/Material Spinner/spinner.js
@@ -0,0 +1,13 @@
+async function spinner() {
+  var spinner = document.createElement("div");
+  spinner.classList.add("spinner-container");
+  spinner.innerHTML = `
+  <svg class="spinner" width="45px" height="45px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
+   <circle class="path" fill="none" stroke-width="6" stroke-linecap="spuare" cx="33" cy="33" r="30"></circle>
+</svg>
+  `;
+  spinnerParent.appendChild(spinner);
+  if (spinnerParent === document.querySelector("#app")) {
+  spinner.classList.add("full-height");
+  }
+}
