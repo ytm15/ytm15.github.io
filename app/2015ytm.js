6eb302179d01e81d861837f0f7638253e3547604
YouTube Mobile 2015 <163555831+ytm15@users.noreply.github.com>
2024-09-21 15:53:36 +0300

    Add files via upload

--- app/2015ytm.js
+++ app/2015ytm.js
@@ -181,16 +181,16 @@ if (HEADER_RED_STYLE_expflag == "false") {
     defaultMetaColor = metaColor3;
     }
     if (APP_DEMATERIALIZE_UI_expflag == "true") {
-    metaColorElm.content = "#000000";
-    defaultMetaColor = "#000000";
+    metaColorElm.content = "transparent";
+    defaultMetaColor = "transparent";
     }
     defaultMetaColor = metaColorElm.content;
 } else {
     metaColorElm.content = metaColor1;
     defaultMetaColor = metaColor1;
     if (APP_DEMATERIALIZE_UI_expflag == "true") {
-    metaColorElm.content = "#000000";
-    defaultMetaColor = "#000000";
+    metaColorElm.content = "transparent";
+    defaultMetaColor = "transparent";
     }
 };
 
@@ -247,7 +247,7 @@ if (headerIsChannel == 'true' && headerBar.querySelector("header").dataset.mode 
 metaColorElm.content = metaColor5;
 }
 if (APP_DEMATERIALIZE_UI_expflag == "true") {
-metaColorElm.content = "#000000";
+metaColorElm.content = "transparent";
 }
 }
 
