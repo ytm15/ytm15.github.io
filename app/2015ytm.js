829d9da21aca4d793be1e5cf47ce1be000f0e77a
ytm15mod <heneryfopaz@gmail.com>
2025-04-11 13:52:43 -0700

    Update 2015ytm.js

--- app/2015ytm.js
+++ app/2015ytm.js
@@ -251,7 +251,7 @@ metaColorElm.content = "#000000";
 }
 }
 
-APIbaseURL = "https://inv.nadeko.net/";
+APIbaseURL = "https://invidious.nerdvpn.de/";
 APIbaseURLWatch = "https://inv.nadeko.net/";
 APIbaseURLNew = "https://yt-api.p.rapidapi.com/";
 APIbaseURLPiped = "https://pipedapi.leptons.xyz/";
