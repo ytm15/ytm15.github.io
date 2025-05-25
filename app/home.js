5e9f6e1c34ffe98dcd52026a56da4157403726fd
YacineYax <117831802+Yacine-Book@users.noreply.github.com>
2025-05-25 17:12:59 +0300

    Update home.js

--- app/home.js
+++ app/home.js
@@ -640,6 +640,7 @@ function renderDataTrending(homeShelfTrendingType, shelfTitle) {
 <span class="error-text">There was an error connecting to the server</span>
 </div>
 <div class="material-button-container" data-style="grey_filled" data-icon-only="false" is-busy="false" aria-busy="false" disabled="false"><button class="material-button has-shadow" aria-label="Retry" onClick="location.reload();"><div class="button-text">Retry</div></button></div>`;
+    if (APP_NEW_ERROR_SCREEN_expflag == "true"){error.innerHTML=newErrorHtml};
         sectLazyList.appendChild(error);
         error.querySelector("button").onclick = function(){
         renderDataTrending(homeShelfTrendingType, shelfTitle);
