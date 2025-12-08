7f9cb576ffb6d6a527b85e6dcc5f886b4e45ba12
legoskid <94498086+legoskid@users.noreply.github.com>
2025-12-08 12:27:51 -0800

    Change subscriptions icon
    I'm doing this in my nwea testing lol

--- app/2015ytm.js
+++ app/2015ytm.js
@@ -1116,7 +1116,7 @@ function renderPivotBar(){
     {
       "name": Subs_text_string,
       "pivotName": "subscriptions",
-      "iconPath": "M20,8H4V6H20V8M18,2H6V4H18V2M22,12V20A2,2 0 0,1 20,22H4A2,2 0 0,1 2,20V12A2,2 0 0,1 4,10H20A2,2 0 0,1 22,12M16,16L10,12.73V19.26L16,16Z",
+      "iconPath": "M20 8H4V6h16v2zm-2-6H6v2h12V2zm4 8v12H2V10h20zm-6 6-6-3.27v6.53L16 16z",
       "link": "subscriptions"
     },
     {
