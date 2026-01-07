01811fc4918384a2046dff084561c3b8cc86e121
legoskid <94498086+legoskid@users.noreply.github.com>
2026-01-07 11:45:44 -0800

    Working camera icon for HEADER_CAST_ALTERNATE_ICON
    make it work

--- app/header.js
+++ app/header.js
@@ -255,7 +255,10 @@ function renderHeader() {
     castBtn.setAttribute("aria-haspopup", "false");
     let alternateIcon = `<path data-glyph="cast-connected" d="M448,384 M21,235 v-43 q52,0,96.5,-26 t70,-70 t25.5,-96 h43 q0,64,-31.5,118 t-85.5,85.5 t-118,31.5 M21,149 v-42 q29,0,53.5,-14.5 t39,-39 t14.5,-53.5 h43 q0,30,-11.5,57.5 t-32.5,48.5 t-48.5,32 t-57.5,11 M21,64 v-64 h64 q0,27,-18.5,45.5 t-45.5,18.5 Z"/>`
     if (HEADER_CAST_ALTERNATE_ICON_expflag == "true") {alternateIcon = `<path data-glyph="cast-connected" fill="#919191" d="M448,384 M21,235 v-43 q52,0,96.5,-26 t70,-70 t25.5,-96 h43 q0,64,-31.5,118 t-85.5,85.5 t-118,31.5 M405,299 h-298 v-35 q63,-21,110.5,-68 t67.5,-111 h120 v214 M21,149 v-42 q29,0,53.5,-14.5 t39,-39 t14.5,-53.5 h43 q0,30,-11.5,57.5 t-32.5,48.5 t-48.5,32 t-57.5,11 M21,64 v-64 h64 q0,27,-18.5,45.5 t-45.5,18.5 Z"/>`}
-    castBtn.innerHTML = `<ytm15-icon class="search-icon"><svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 64.7273 512 448.462"><g transform="scale(1,-1) translate(0, -480.36365)"><path data-glyph="cast-connected" d="M448,384 h-384 q-18,0,-30.5,-12.5 t-12.5,-30.5 v-64 h43 v64 h384 v-298 h-149 v-43 h149 q18,0,30.5,12.5 t12.5,30.5 v298 q0,18,-12.5,30.5 t-30.5,12.5"/>` + alternateIcon + `</g></svg></ytm15-icon>`;
+    if (HEADER_CAST_ALTERNATE_ICON_expflag == "Camera") {
+        castBtn.innerHTML = `<ytm15-icon class="search-icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M17 10.5V7c0-.55-.45-1-1-1H4c-.55 0-1 .45-1 1v10c0 .55.45 1 1 1h12c.55 0 1-.45 1-1v-3.5l4 4v-11z"/></svg></ytm15-icon>`;}
+    else {
+    castBtn.innerHTML = `<ytm15-icon class="search-icon"><svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 64.7273 512 448.462"><g transform="scale(1,-1) translate(0, -480.36365)"><path data-glyph="cast-connected" d="M448,384 h-384 q-18,0,-30.5,-12.5 t-12.5,-30.5 v-64 h43 v64 h384 v-298 h-149 v-43 h149 q18,0,30.5,12.5 t12.5,30.5 v298 q0,18,-12.5,30.5 t-30.5,12.5"/>` + alternateIcon + `</g></svg></ytm15-icon>`;}
     if (window.location.pathname.split("/").slice(3, 4) == "results.html") {
         castBtn.setAttribute("hidden", "");
     }
