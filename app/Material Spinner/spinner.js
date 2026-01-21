a52aea1053e1e948235a11ec7679d0c0135adcb8
legoskid <94498086+legoskid@users.noreply.github.com>
2026-01-21 12:08:49 -0800

    Make iOS spinner more accurate
    thinner, lower down on the page

--- app/Material Spinner/spinner.js
+++ app/Material Spinner/spinner.js
@@ -3,8 +3,8 @@ function spinner() {
   spinner.classList.add("spinner-container");
   if (localStorage.getItem("WEB_IOS_SPINNER") == "true") {
     spinner.innerHTML = `
-    <svg class="spinner" width="30px" height="30px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
-    <circle class="path" fill="none" stroke-width="7" stroke-linecap="spuare" cx="33" cy="33" r="30"></circle>
+    <svg style="margin-top:5rem;" class="spinner" width="30px" height="30px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
+    <circle class="path" fill="none" stroke-width="6" stroke-linecap="spuare" cx="33" cy="33" r="30"></circle>
     </svg>
     `;
   } else {
