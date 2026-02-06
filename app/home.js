b39025aeff2f7def784d88e41ccb445754b580d8
legoskid <94498086+legoskid@users.noreply.github.com>
2026-02-06 12:41:57 -0800

    Proper subscriptions placeholder
    similar to the logged out one

--- app/home.js
+++ app/home.js
@@ -883,7 +883,7 @@ function subscriptionsPage() {
 
     const sectLazyList = document.createElement("div");
     sectLazyList.classList.add('lazy-list');
-    sectLazyList.innerHTML = `(Local) Subscriptions are coming to YTm15 Soon!`;
+    sectLazyList.innerHTML = `<center><svg viewBox="0 0 24 24" style="width: auto;padding: 5rem;max-height: 300px;" fill="#ddd"><path d="M20 8H4V6h16v2zm-2-6H6v2h12V2zm4 8v12H2V10h20zm-6 6-6-3.27v6.53L16 16z"></path></svg></center><p style="text-align: center;color: #333;">(Local) Subscriptions are coming to YTm15 Soon!</p>`;
     section.appendChild(sectLazyList);
 
     const parent = document.querySelector(".page-container");
