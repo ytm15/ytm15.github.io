6e509ba5facc8d9b386b77416b617117c7a1119e
legoskid <94498086+legoskid@users.noreply.github.com>
2026-04-24 14:12:53 -0700

    Explore Icon in Top Header
    2015 header

--- app/home.js
+++ app/home.js
@@ -75,6 +75,7 @@ function renderData() {
     }
     tab1.href = "#/trending";
     tab1.innerHTML = `<img class="ytm15-img-icon ytm15-img home-icon" src="ic_tab_trending.png"></img>`
+    if (PIVOT_TRENDING_IS_EXPLORE_expflag == "true") {tab1.innerHTML = `<svg class="ytm15-img-icon ytm15-img home-icon" viewBox="0 0 24 24"><path d="M14.19,14.19L6,18L9.81,9.81L18,6M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M12,10.9A1.1,1.1 0 0,0 10.9,12A1.1,1.1 0 0,0 12,13.1A1.1,1.1 0 0,0 13.1,12A1.1,1.1 0 0,0 12,10.9Z" fill="#white"></path></svg>`;};
 
     const tBTabCont2 = document.createElement("div");
     tBTabCont2.classList.add("tabbar-tab-container", "subscriptions-tab");
